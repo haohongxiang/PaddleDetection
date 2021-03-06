8e40328709ecc1187a29d855ff834cf3034f0364
lyuwenyu <wenyu.lyu@gmail.com>
2021-03-06 14:16:33 +0800

    update

--- dygraph/ppdet/engine/trainer.py
+++ dygraph/ppdet/engine/trainer.py
@@ -63,7 +63,7 @@ class Trainer(object):
         # build data loader
         self.dataset = cfg['{}Dataset'.format(self.mode.capitalize())]
         if self.mode == 'train':
-            self.loader = create('{}Reader'.format(self.mode.capitalize()))(self.dataset, 1)
+            self.loader = create('{}Reader'.format(self.mode.capitalize()))(self.dataset, cfg.worker_num)
 
         # EvalDataset build with BatchSampler to evaluate in single device
         # TODO: multi-device evaluate
