f3baab8da8e87a9df73ebd61d8ca283ed9f0c953
lyuwenyu <wenyu.lyu@gmail.com>
2021-03-16 13:21:37 +0800

    update mosaic

--- dygraph/ppdet/data/transform/operators.py
+++ dygraph/ppdet/data/transform/operators.py
@@ -1919,23 +1919,32 @@ class Poly2Mask(BaseOperator):
         return sample
 
 
-
 @register_op
 class Mosaic(BaseOperator):
-    def __init__(self, img_size=640, scale=0.5, translate=0.1, degree=0.0, shear=0., perspective=0., debug=False):
+    def __init__(self,
+                 img_size=640,
+                 scale=0.5,
+                 translate=0.1,
+                 degree=0.0,
+                 shear=0.,
+                 perspective=0.,
+                 debug=False):
         """ Mosaic
         """
         super(Mosaic, self).__init__()
         self.img_size = img_size
         self.debug = debug
 
-        self.scale = scale 
+        self.scale = scale
         self.degree = degree
         self.translate = translate
         self.shear = shear
         self.perspective = perspective
-        
-        
+
+        self.hsv_h = 0.015
+        self.hsv_s = 0.7
+        self.hsv_v = 0.4
+
     def __call__(self, sample, context=None):
         if not isinstance(sample, Sequence):
             return sample
@@ -1956,7 +1965,6 @@ class Mosaic(BaseOperator):
 
         return result
 
-
     def load_mosaic(self, sample, img_size=640):
         '''load mosaic
         '''
@@ -1964,12 +1972,12 @@ class Mosaic(BaseOperator):
             return sample
 
         assert len(sample) == 4, 'mosaic need four samples'
-        
+
         mosaic_border = [-img_size // 2, -img_size // 2]
 
         labels4 = []
         s = img_size
-        yc, xc = [int(random.uniform(-x, 2 * s + x)) for x in mosaic_border] 
+        yc, xc = [int(random.uniform(-x, 2 * s + x)) for x in mosaic_border]
 
         for i, blob in enumerate(sample):
 
@@ -1979,9 +1987,13 @@ class Mosaic(BaseOperator):
 
             # place img in img4
             if i == 0:  # top left
-                img4 = np.full((s * 2, s * 2, 3), 114, dtype=np.uint8)  # base image with 4 tiles
-                x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
-                x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
+                img4 = np.full(
+                    (s * 2, s * 2, 3), 114,
+                    dtype=np.uint8)  # base image with 4 tiles
+                x1a, y1a, x2a, y2a = max(xc - w, 0), max(
+                    yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
+                x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (
+                    y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
             elif i == 1:  # top right
                 x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
                 x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
@@ -1989,47 +2001,52 @@ class Mosaic(BaseOperator):
                 x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
                 x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, w, min(y2a - y1a, h)
             elif i == 3:  # bottom right
-                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
+                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2,
+                                                                     yc + h)
                 x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
 
-            img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b] 
-            
+            img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]
+
             padw = x1a - x1b
             padh = y1a - y1b
 
             if labels.size:
                 labels[:, 1:] = self.xywhn2xyxy(labels[:, 1:], w, h, padw, padh)
-                
+
             labels4.append(labels)
-            
+
         labels4 = np.concatenate(labels4, 0)
         for x in labels4[:, 1:]:
             np.clip(x, 0, 2 * s, out=x)
-        
-        img4, labels4 = self.random_perspective(img=img4, targets=labels4,
-                                                degrees=self.degree,
-                                                translate=self.translate,
-                                                scale=self.scale,
-                                                shear=self.shear,
-                                                perspective=self.perspective,
-                                                border=mosaic_border)  # border to remove
+
+        img4, labels4 = self.random_perspective(
+            img=img4,
+            targets=labels4,
+            degrees=self.degree,
+            translate=self.translate,
+            scale=self.scale,
+            shear=self.shear,
+            perspective=self.perspective,
+            border=mosaic_border)  # border to remove
+
+        if self.hsv_h > 0 or self.hsv_s > 0 or self.hsv_v > 0:
+            self.augment_hsv(img4, self.hsv_h, self.hsv_s, self.hsv_v)
 
         return img4, labels4
-
 
     @staticmethod
     def show(img, bbox, name='', root='./output/'):
         '''for debug
-        '''        
+        '''
         img = Image.fromarray(img)
         draw = ImageDraw.Draw(img)
-        
+
         if not len(bbox):
-            return 
+            return
 
         for _, b in enumerate(bbox):
             draw.rectangle(tuple(b[1:]), outline='red')
-        
+
         if not root:
             root = './debug/'
             os.mkdir(root)
@@ -2037,9 +2054,8 @@ class Mosaic(BaseOperator):
         if not name:
             import time
             name = str(time.time()) + '.jpg'
-    
-        img.save(os.path.join(root, name))
 
+        img.save(os.path.join(root, name))
 
     @staticmethod
     def xywhn2xyxy(x, w=640, h=640, padw=0, padh=0):
@@ -2050,9 +2066,8 @@ class Mosaic(BaseOperator):
         y[:, 1] = h * (x[:, 1] - x[:, 3] / 2) + padh  # top left y
         y[:, 2] = w * (x[:, 0] + x[:, 2] / 2) + padw  # bottom right x
         y[:, 3] = h * (x[:, 1] + x[:, 3] / 2) + padh  # bottom right y
-        
+
         return y
-    
 
     @staticmethod
     def xyxyn2xywh(bbox, ):
@@ -2064,7 +2079,6 @@ class Mosaic(BaseOperator):
 
         return bbox
 
-
     @staticmethod
     def normbbox(bbox, width, height):
         '''0-1
@@ -2075,7 +2089,6 @@ class Mosaic(BaseOperator):
 
         return bbox
 
-
     @staticmethod
     def resize_image(img, img_size):
         '''resize image
@@ -2084,22 +2097,30 @@ class Mosaic(BaseOperator):
         r = img_size / max(h0, w0)  # resize image to img_size
         if r != 1:  # always resize down, only resize up if training with augmentation
             # interp = cv2.INTER_AREA if r < 1 and not self.augment else cv2.INTER_LINEAR
-            img = cv2.resize(img, (int(w0 * r), int(h0 * r)), interpolation=cv2.INTER_LINEAR)
-            
+            img = cv2.resize(
+                img, (int(w0 * r), int(h0 * r)), interpolation=cv2.INTER_LINEAR)
+
         return img, (h0, w0), img.shape[:2]  # img, hw_original, hw_resized
-        
 
     @staticmethod
-    def box_candidates(box1, box2, wh_thr=2, ar_thr=20, area_thr=0.1, eps=1e-16):  # box1(4,n), box2(4,n)
+    def box_candidates(box1, box2, wh_thr=2, ar_thr=20, area_thr=0.1,
+                       eps=1e-16):  # box1(4,n), box2(4,n)
         # Compute candidate boxes: box1 before augment, box2 after augment, wh_thr (pixels), aspect_ratio_thr, area_ratio
         w1, h1 = box1[2] - box1[0], box1[3] - box1[1]
         w2, h2 = box2[2] - box2[0], box2[3] - box2[1]
         ar = np.maximum(w2 / (h2 + eps), h2 / (w2 + eps))  # aspect ratio
-        return (w2 > wh_thr) & (h2 > wh_thr) & (w2 * h2 / (w1 * h1 + eps) > area_thr) & (ar < ar_thr)  # candidates
-
+        return (w2 > wh_thr) & (h2 > wh_thr) & (
+            w2 * h2 / (w1 * h1 + eps) > area_thr) & (ar < ar_thr)  # candidates
 
     @staticmethod
-    def random_perspective(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10, perspective=0.0, border=(0, 0)):
+    def random_perspective(img,
+                           targets=(),
+                           degrees=10,
+                           translate=.1,
+                           scale=.1,
+                           shear=10,
+                           perspective=0.0,
+                           border=(0, 0)):
         '''augmentation
         '''
         height = img.shape[0] + border[0] * 2  # shape(h,w,c)
@@ -2112,8 +2133,10 @@ class Mosaic(BaseOperator):
 
         # Perspective
         P = np.eye(3)
-        P[2, 0] = random.uniform(-perspective, perspective)  # x perspective (about y)
-        P[2, 1] = random.uniform(-perspective, perspective)  # y perspective (about x)
+        P[2, 0] = random.uniform(-perspective,
+                                 perspective)  # x perspective (about y)
+        P[2, 1] = random.uniform(-perspective,
+                                 perspective)  # y perspective (about x)
 
         # Rotation and Scale
         R = np.eye(3)
@@ -2125,43 +2148,73 @@ class Mosaic(BaseOperator):
 
         # Shear
         S = np.eye(3)
-        S[0, 1] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # x shear (deg)
-        S[1, 0] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # y shear (deg)
+        S[0, 1] = math.tan(random.uniform(-shear, shear) * math.pi /
+                           180)  # x shear (deg)
+        S[1, 0] = math.tan(random.uniform(-shear, shear) * math.pi /
+                           180)  # y shear (deg)
 
         # Translation
         T = np.eye(3)
-        T[0, 2] = random.uniform(0.5 - translate, 0.5 + translate) * width  # x translation (pixels)
-        T[1, 2] = random.uniform(0.5 - translate, 0.5 + translate) * height  # y translation (pixels)
+        T[0, 2] = random.uniform(
+            0.5 - translate, 0.5 + translate) * width  # x translation (pixels)
+        T[1, 2] = random.uniform(
+            0.5 - translate, 0.5 + translate) * height  # y translation (pixels)
 
         # Combined rotation matrix
-        M = T @ S @ R @ P @ C  # order of operations (right to left) is IMPORTANT
-        if (border[0] != 0) or (border[1] != 0) or (M != np.eye(3)).any():  # image changed
+        M = T @S @R @P @C  # order of operations (right to left) is IMPORTANT
+        if (border[0] != 0) or (border[1] != 0) or (
+                M != np.eye(3)).any():  # image changed
             if perspective:
-                img = cv2.warpPerspective(img, M, dsize=(width, height), borderValue=(114, 114, 114))
+                img = cv2.warpPerspective(
+                    img, M, dsize=(width, height), borderValue=(114, 114, 114))
             else:  # affine
-                img = cv2.warpAffine(img, M[:2], dsize=(width, height), borderValue=(114, 114, 114))
+                img = cv2.warpAffine(
+                    img,
+                    M[:2],
+                    dsize=(width, height),
+                    borderValue=(114, 114, 114))
 
         # Transform label coordinates
         n = len(targets)
         if n:
             new = np.zeros((n, 4))
             xy = np.ones((n * 4, 3))
-            xy[:, :2] = targets[:, [1, 2, 3, 4, 1, 4, 3, 2]].reshape(n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
-            xy = xy @ M.T  # transform
-            xy = (xy[:, :2] / xy[:, 2:3] if perspective else xy[:, :2]).reshape(n, 8)  # perspective rescale or affine
+            xy[:, :2] = targets[:, [1, 2, 3, 4, 1, 4, 3, 2]].reshape(
+                n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
+            xy = xy @M.T  # transform
+            xy = (xy[:, :2] / xy[:, 2:3] if perspective else xy[:, :2]).reshape(
+                n, 8)  # perspective rescale or affine
 
             # create new boxes
             x = xy[:, [0, 2, 4, 6]]
             y = xy[:, [1, 3, 5, 7]]
-            new = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
+            new = np.concatenate(
+                (x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
 
             # clip
             new[:, [0, 2]] = new[:, [0, 2]].clip(0, width)
             new[:, [1, 3]] = new[:, [1, 3]].clip(0, height)
 
             # filter candidates
-            i = Mosaic.box_candidates(box1=targets[:, 1:5].T * s, box2=new.T, area_thr=0.10)
+            i = Mosaic.box_candidates(
+                box1=targets[:, 1:5].T * s, box2=new.T, area_thr=0.10)
             targets = targets[i]
             targets[:, 1:5] = new[i]
 
         return img, targets
+
+    @staticmethod
+    def augment_hsv(img, hgain=0.5, sgain=0.5, vgain=0.5):
+        r = np.random.uniform(-1, 1,
+                              3) * [hgain, sgain, vgain] + 1  # random gains
+        hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+        dtype = img.dtype  # uint8
+
+        x = np.arange(0, 256, dtype=np.int16)
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+
+        img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat),
+                             cv2.LUT(val, lut_val))).astype(dtype)
+        cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
