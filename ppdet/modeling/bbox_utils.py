8238dcb7e2ff7b10f1f4db4d5e0bb6266e59f1ee
FL77N <feizzhang@163.com>
2021-06-23 15:27:35 +0800

    add sparsercnn

--- ppdet/modeling/bbox_utils.py
+++ ppdet/modeling/bbox_utils.py
@@ -154,6 +154,79 @@ def xywh2xyxy(box):
     return [x1, y1, x2, y2]
 
 
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return paddle.stack(b, axis=-1)
+
+
+def box_xyxy_to_cxcywh(x):
+    x0, y0, x1, y1 = x.unbind(-1)
+    b = [(x0 + x1) / 2, (y0 + y1) / 2,
+         (x1 - x0), (y1 - y0)]
+    return paddle.stack(b, axis=-1)
+
+
+def box_area(boxes):
+    assert (boxes[:, 2:] >= boxes[:, :2]).all()
+    wh = boxes[:, 2:] - boxes[:, :2]
+    return wh[:, 0] * wh[:, 1]
+
+
+def boxes_iou(boxes1, boxes2):
+    '''
+    Compute iou
+
+    Args:
+        boxes1 (paddle.tensor) shape (N, 4)
+        boxes2 (paddle.tensor) shape (M, 4)
+
+    Return:
+        (paddle.tensor) shape (N, M)
+    '''
+    area1 = box_area(boxes1)
+    area2 = box_area(boxes2)
+
+    lt = paddle.maximum(boxes1.unsqueeze(-2)[:, :, :2], boxes2[:, :2])
+    rb = paddle.minimum(boxes1.unsqueeze(-2)[:, :, 2:], boxes2[:, 2:])
+
+    wh = (rb - lt).astype("float32").clip(min=1e-9)
+    inter = wh[:, :, 0] * wh[:, :, 1]
+
+    union = area1.unsqueeze(-1) + area2 - inter + 1e-9
+
+    iou = inter / union
+    return iou, union
+
+
+def get_bboxes_giou(boxes1, boxes2, eps=1e-9):
+    """calculate the ious of boxes1 and boxes2
+
+    Args:
+        boxes1 (Tensor): shape [N, 4]
+        boxes2 (Tensor): shape [M, 4]
+        eps (float): epsilon to avoid divide by zero
+
+    Return:
+        ious (Tensor): ious of boxes1 and boxes2, with the shape [N, M]
+    """
+    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
+    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+
+    iou, union = boxes_iou(boxes1, boxes2)
+
+    lt = paddle.minimum(boxes1.unsqueeze(-2)[:, :, :2], boxes2[:, :2])
+    rb = paddle.maximum(boxes1.unsqueeze(-2)[:, :, 2:], boxes2[:, 2:])
+
+    wh = (rb - lt).astype("float32").clip(min=eps)
+    enclose_area = wh[:, :, 0] * wh[:, :, 1]
+
+    giou = iou - (enclose_area - union) / enclose_area
+
+    return giou
+
+
 def make_grid(h, w, dtype):
     yv, xv = paddle.meshgrid([paddle.arange(h), paddle.arange(w)])
     return paddle.stack((xv, yv), 2).cast(dtype=dtype)
