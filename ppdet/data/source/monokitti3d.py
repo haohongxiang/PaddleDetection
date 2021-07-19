b2691d29f28c89616f84afe345ad35e9d57fa5fe
lyuwenyu <wenyu.lyu@gmail.com>
2021-07-19 17:38:14 +0800

    kitti 3d

---
+++ ppdet/data/source/monokitti3d.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved. 
+#   
+# Licensed under the Apache License, Version 2.0 (the "License");   
+# you may not use this file except in compliance with the License.  
+# You may obtain a copy of the License at   
+#   
+#     http://www.apache.org/licenses/LICENSE-2.0    
+#   
+# Unless required by applicable law or agreed to in writing, software   
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+# See the License for the specific language governing permissions and   
+# limitations under the License.
+
+import os
+import numpy as np
+from ppdet.core.workspace import register, serializable
+from .dataset import DetDataset
+
+from ppdet.utils.logger import setup_logger
+logger = setup_logger(__name__)
+
+
+@register
+@serializable
+class MonoKitti3d(DetDataset):
+    def __init__(self, dataset_dir, image_dir, anno_path, data_fields,
+                 sample_num, use_default_label, **kwargs):
+        super().__init__(
+            dataset_dir=dataset_dir,
+            image_dir=image_dir,
+            anno_path=anno_path,
+            data_fields=data_fields,
+            sample_num=sample_num,
+            use_default_label=use_default_label,
+            **kwargs)
+        pass
+
+    def parse_dataset(self):
+        pass
+
+        self.roidbs = None
