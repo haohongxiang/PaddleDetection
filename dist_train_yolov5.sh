8ec6be3fdf3990bd405a2001171a69b47f5cb465
lyuwenyu <wenyu.lyu@gmail.com>
2021-08-15 04:09:42 +0000

    update

--- dist_train_yolov5.sh
+++ dist_train_yolov5.sh
@@ -4,5 +4,5 @@ ps aux | grep tools/train.py | awk '{print $2}' | xargs kill -9
 
 sleep 3
 
-python -m paddle.distributed.launch --log_dir=./yolov5$1/ --gpus 0,1,2,3 tools/train.py -c configs/yolov5/yolov5$1_coco.yml
+python -m paddle.distributed.launch --log_dir=./yolov5$1/ --gpus 0,1,2,3,4,5,6,7 tools/train.py -c configs/yolov5/yolov5$1_coco.yml
 
