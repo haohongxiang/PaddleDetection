1195813fb735d1ebeda46c94c2c51a72f7afb1ba
lyuwenyu <wenyu.lyu@gmail.com>
2021-08-25 09:38:56 +0000

    update

--- dist_train_lst.sh
+++ dist_train_lst.sh
@@ -1,6 +1,11 @@
 
 ps aux | grep tools/train.py | awk '{print $2}' | xargs kill -9
 
+
+sleep 3
+
+rm -rf /dev/shm/paddle*
+
 sleep 3
 
 python -m paddle.distributed.launch --log_dir=../logs --selected_gpu 0,1,2,3,4,5,6,7 tools/train.py -c configs/ppyolo/lst_ppyolo_r50vd_dcn.yml --eval
