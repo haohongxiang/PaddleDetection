4d5370c7ec08a4ee2980f77e2b43108a92a709e0
lyuwenyu <wenyu.lyu@gmail.com>
2021-09-16 03:24:07 +0000

    update

--- dist_train_lst.sh
+++ dist_train_lst.sh
@@ -8,5 +8,5 @@ rm -rf /dev/shm/paddle*
 
 sleep 3
 
-python -m paddle.distributed.launch --log_dir=../logs --selected_gpu 0,1,2,3,4,5,6,7 tools/train.py -c configs/ppyolo/lst_ppyolo_r50vd_dcn.yml --eval
-
+# python -m paddle.distributed.launch --log_dir=../logs --selected_gpu 0,1,2,3,4,5,6,7 tools/train.py -c configs/ppyolo/lst_ppyolo_r50vd_dcn.yml --eval
+python -m paddle.distributed.launch --log_dir=../logs --selected_gpu 0,1,2,3,4,5,6,7 tools/train.py -c configs/ppyolo/lst_ppyolo_r50vd_dcn.yml
