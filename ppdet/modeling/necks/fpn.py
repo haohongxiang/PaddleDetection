5960c92cf150ecea0a2f42056ba86c95761783d7
lyuwenyu <wenyu.lyu@gmail.com>
2021-09-24 05:24:20 +0000

    update

--- ppdet/modeling/necks/fpn.py
+++ ppdet/modeling/necks/fpn.py
@@ -21,7 +21,7 @@ from ppdet.core.workspace import register, serializable
 from ppdet.modeling.layers import ConvNormLayer
 from ..shape_spec import ShapeSpec
 
-__all__ = ['FPN']
+__all__ = ['FPN', 'FPNL']
 
 
 @register
@@ -65,7 +65,8 @@ class FPN(nn.Layer):
                  norm_type=None,
                  norm_decay=0.,
                  freeze_norm=False,
-                 relu_before_extra_convs=True):
+                 relu_before_extra_convs=True, 
+                 use_scale_attention=False,):
         super(FPN, self).__init__()
         self.out_channel = out_channel
         for s in range(extra_stage):
@@ -174,7 +175,247 @@ class FPN(nn.Layer):
                             weight_attr=ParamAttr(
                                 initializer=XavierUniform(fan_out=fan))))
                 self.fpn_convs.append(extra_fpn_conv)
+        
+        if use_scale_attention:
+            self.use_scale_attention = use_scale_attention
+            self.scale_attention = ScaleAttention(len(spatial_scales) + extra_stage)
+            
+            
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        return {
+            'in_channels': [i.channels for i in input_shape],
+            'spatial_scales': [1.0 / i.stride for i in input_shape],
+        }
 
+    def forward(self, body_feats):
+        laterals = []
+        num_levels = len(body_feats)
+        for i in range(num_levels):
+            laterals.append(self.lateral_convs[i](body_feats[i]))
+
+        for i in range(1, num_levels):
+            lvl = num_levels - i
+            upsample = F.interpolate(
+                laterals[lvl],
+                scale_factor=2.,
+                mode='nearest', )
+            laterals[lvl - 1] += upsample
+
+        fpn_output = []
+        for lvl in range(num_levels):
+            fpn_output.append(self.fpn_convs[lvl](laterals[lvl]))
+
+        if self.extra_stage > 0:
+            # use max pool to get more levels on top of outputs (Faster R-CNN, Mask R-CNN)
+            if not self.has_extra_convs:
+                assert self.extra_stage == 1, 'extra_stage should be 1 if FPN has not extra convs'
+                fpn_output.append(F.max_pool2d(fpn_output[-1], 1, stride=2))
+            # add extra conv levels for RetinaNet(use_c5)/FCOS(use_p5)
+            else:
+                if self.use_c5:
+                    extra_source = body_feats[-1]
+                else:
+                    extra_source = fpn_output[-1]
+                fpn_output.append(self.fpn_convs[num_levels](extra_source))
+
+                for i in range(1, self.extra_stage):
+                    if self.relu_before_extra_convs:
+                        fpn_output.append(self.fpn_convs[num_levels + i](F.relu(
+                            fpn_output[-1])))
+                    else:
+                        fpn_output.append(self.fpn_convs[num_levels + i](
+                            fpn_output[-1]))
+        
+        # print(([out.shape for out in fpn_output]))
+        if self.use_scale_attention: 
+            fpn_output = self.scale_attention(fpn_output)
+
+        return fpn_output
+
+    @property
+    def out_shape(self):
+        return [
+            ShapeSpec(
+                channels=self.out_channel, stride=1. / s)
+            for s in self.spatial_scales
+        ]
+
+    
+
+import paddle
+import paddle.nn as nn
+
+class HardSigmoid(nn.Layer):
+    def __init__(self, ):
+        super(HardSigmoid, self).__init__()
+
+    def forward(self, x):
+        x = paddle.maximum(paddle.zeros_like(x), paddle.minimum(paddle.ones_like(x), (x + 1) / 2.))
+        return x
+
+    
+
+class ScaleAttention(nn.Layer):
+    def __init__(self, scales=3):
+        super(ScaleAttention, self).__init__()
+        self.pool2d = nn.AdaptiveAvgPool2D(output_size=1)
+        # self.linear = nn.Sequential(nn.Linear(scales, scales), nn.ReLU())
+        self.conv2d = nn.Sequential(nn.Conv2D(scales, scales, 1, 1), nn.ReLU())
+        self.hard_sigmoid = lambda x: paddle.maximum(paddle.zeros_like(x), paddle.minimum(paddle.ones_like(x), (x + 1) / 2.))
+        self.scales = scales
+        
+    def forward(self, feats):
+        assert len(feats) == self.scales, ''
+        shapes = [x.shape for x in feats]
+        
+        outs = [x.reshape([shapes[i][0], 1, shapes[i][1], -1]) for i, x in enumerate(feats)]
+        outs = [self.pool2d(x) for x in  outs]
+        outs = paddle.concat(outs, axis=1) # n, scale, 1, 1
+        outs = self.conv2d(outs) # n, scale, 1, 1
+        # outs = self.linear(outs.squeeze()) # n, scale
+        outs = self.hard_sigmoid(outs)
+
+        return [x * outs[:, i:i+1]  for i, x in enumerate(feats)]
+        
+
+
+class ChannelAttention(nn.Layer):
+    def __init__(self, ):
+        super().__init__()
+    
+    def forward(self, x):
+        pass
+    
+    
+
+
+@register
+@serializable
+class FPNL(nn.Layer):
+    def __init__(self,
+                 in_channels,
+                 out_channel,
+                 spatial_scales=[0.25, 0.125, 0.0625, 0.03125],
+                 has_extra_convs=False,
+                 extra_stage=1,
+                 use_c5=True,
+                 norm_type=None,
+                 norm_decay=0.,
+                 freeze_norm=False,
+                 relu_before_extra_convs=True):
+        super(FPNL, self).__init__()
+        self.out_channel = out_channel
+        for s in range(extra_stage):
+            spatial_scales = spatial_scales + [spatial_scales[-1] / 2.]
+        self.spatial_scales = spatial_scales
+        self.has_extra_convs = has_extra_convs
+        self.extra_stage = extra_stage
+        self.use_c5 = use_c5
+        self.relu_before_extra_convs = relu_before_extra_convs
+        self.norm_type = norm_type
+        self.norm_decay = norm_decay
+        self.freeze_norm = freeze_norm
+
+        self.lateral_convs = []
+        self.fpn_convs = []
+        fan = out_channel * 3 * 3
+
+        # stage index 0,1,2,3 stands for res2,res3,res4,res5 on ResNet Backbone
+        # 0 <= st_stage < ed_stage <= 3
+        st_stage = 4 - len(in_channels)
+        ed_stage = st_stage + len(in_channels) - 1
+        for i in range(st_stage, ed_stage + 1):
+            if i == 3:
+                lateral_name = 'fpn_inner_res5_sum'
+            else:
+                lateral_name = 'fpn_inner_res{}_sum_lateral'.format(i + 2)
+            in_c = in_channels[i - st_stage]
+            if self.norm_type is not None:
+                lateral = self.add_sublayer(
+                    lateral_name,
+                    ConvNormLayer(
+                        ch_in=in_c,
+                        ch_out=out_channel,
+                        filter_size=1,
+                        stride=1,
+                        norm_type=self.norm_type,
+                        norm_decay=self.norm_decay,
+                        freeze_norm=self.freeze_norm,
+                        initializer=XavierUniform(fan_out=in_c)))
+            else:
+                lateral = self.add_sublayer(
+                    lateral_name,
+                    nn.Conv2D(
+                        in_channels=in_c,
+                        out_channels=out_channel,
+                        kernel_size=1,
+                        weight_attr=ParamAttr(
+                            initializer=XavierUniform(fan_out=in_c))))
+            self.lateral_convs.append(lateral)
+
+            fpn_name = 'fpn_res{}_sum'.format(i + 2)
+            if self.norm_type is not None:
+                fpn_conv = self.add_sublayer(
+                    fpn_name,
+                    ConvNormLayer(
+                        ch_in=out_channel,
+                        ch_out=out_channel,
+                        filter_size=3,
+                        stride=1,
+                        norm_type=self.norm_type,
+                        norm_decay=self.norm_decay,
+                        freeze_norm=self.freeze_norm,
+                        initializer=XavierUniform(fan_out=fan)))
+            else:
+                fpn_conv = self.add_sublayer(
+                    fpn_name,
+                    nn.Conv2D(
+                        in_channels=out_channel,
+                        out_channels=out_channel,
+                        kernel_size=3,
+                        padding=1,
+                        weight_attr=ParamAttr(
+                            initializer=XavierUniform(fan_out=fan))))
+            self.fpn_convs.append(fpn_conv)
+
+        # add extra conv levels for RetinaNet(use_c5)/FCOS(use_p5)
+        if self.has_extra_convs:
+            for i in range(self.extra_stage):
+                lvl = ed_stage + 1 + i
+                if i == 0 and self.use_c5:
+                    in_c = in_channels[-1]
+                else:
+                    in_c = out_channel
+                extra_fpn_name = 'fpn_{}'.format(lvl + 2)
+                if self.norm_type is not None:
+                    extra_fpn_conv = self.add_sublayer(
+                        extra_fpn_name,
+                        ConvNormLayer(
+                            ch_in=in_c,
+                            ch_out=out_channel,
+                            filter_size=3,
+                            stride=2,
+                            norm_type=self.norm_type,
+                            norm_decay=self.norm_decay,
+                            freeze_norm=self.freeze_norm,
+                            initializer=XavierUniform(fan_out=fan)))
+                else:
+                    extra_fpn_conv = self.add_sublayer(
+                        extra_fpn_name,
+                        nn.Conv2D(
+                            in_channels=in_c,
+                            out_channels=out_channel,
+                            kernel_size=3,
+                            stride=2,
+                            padding=1,
+                            weight_attr=ParamAttr(
+                                initializer=XavierUniform(fan_out=fan))))
+                self.fpn_convs.append(extra_fpn_conv)
+        
+
+
+                
     @classmethod
     def from_config(cls, cfg, input_shape):
         return {
