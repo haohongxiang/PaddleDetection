7a6fce78d0e6fc9a6b5f0b7530bdc6d4ce46f731
lyuwenyu <wenyu.lyu@gmail.com>
2021-10-27 17:27:55 +0800

    update

--- train.sh
+++ train.sh
@@ -6,3 +6,8 @@ ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9
 
 # python -m paddle.distributed.launch --log_dir=log   --gpus 4,5,6,7 tools/train.py -c configs/picodet/pp_fcos.yml --eval
  python -m paddle.distributed.launch --log_dir=log_csp   --gpus 4,5,6,7 tools/train.py -c configs/picodet/pp_fcos_cspdarknet.yml --eval
+
+
+
+# python tools/export_model.py -c configs/picodet/pp_fcos_cspdarknet.yml
+# python deploy/python/infer.py --model_dir=output_inference/pp_fcos_cspdarknet/ --image_dir=./demo/ --run_mode=fluid --device=GPU --threshold=0.5 
