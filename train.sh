818c8cfbd539d51855fc55c8df36e10bd70f7fcd
lyuwenyu <wenyu.lyu@gmail.com>
2021-11-10 16:57:19 +0800

    update

---
+++ train.sh
@@ -0,0 +1,8 @@
+
+
+
+ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9 
+
+# python -m paddle.distributed.launch --log_dir=log   --gpus 4,5,6,7 tools/train.py -c configs/picodet/pp_fcos.yml --eval
+ python -m paddle.distributed.launch --log_dir=log_yolov5 --gpus 4,5,6,7 tools/train.py -c configs/yolov5/yolov5l_coco.yml --eval
+
