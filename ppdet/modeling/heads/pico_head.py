95209356ddca3f22b19ba52be4510e0fcb9a1c43
lyuwenyu <wenyu.lyu@gmail.com>
2021-11-16 16:47:24 +0800

    stem

--- ppdet/modeling/heads/pico_head.py
+++ ppdet/modeling/heads/pico_head.py
@@ -23,6 +23,7 @@ import paddle.nn as nn
 import paddle.nn.functional as F
 from paddle import ParamAttr
 from paddle.nn.initializer import Normal, Constant
+from PaddleDetection.ppdet.modeling.ops import mish
 
 from ppdet.core.workspace import register
 from ppdet.modeling.layers import ConvNormLayer
@@ -385,10 +386,13 @@ class PicoFeatL(nn.Layer):
         self.negative_slope = negative_slope
 
     def act_func(self, x):
-        if self.act == "leaky_relu":
+        if self.act == "leaky_relu" or self.act == 'leaky':
             x = F.leaky_relu(x, negative_slope=self.negative_slope)
         elif self.act == "hard_swish":
             x = F.hardswish(x)
+        elif self.act == 'mish':
+            x = x * paddle.tanh(F.softplus(x))
+
         return x
 
     def forward(self, fpn_feat, stage_idx):
@@ -655,6 +659,177 @@ class FeatHeadX(nn.Layer):
             return cls_feat, reg_feat
 
         return cls_feat, cls_feat
+
+
+@register
+class FeatHeadL(nn.Layer):
+    """
+    PicoFeat of PicoDet
+
+    Args:
+        feat_in (int): The channel number of input Tensor.
+        feat_out (int): The channel number of output Tensor.
+        num_convs (int): The convolution number of the LiteGFLFeat.
+        norm_type (str): Normalization type, 'bn'/'sync_bn'/'gn'.
+    """
+
+    def __init__(self,
+                 feat_in=256,
+                 feat_out=96,
+                 num_fpn_stride=3,
+                 num_convs=2,
+                 norm_type='bn',
+                 share_cls_reg=False,
+                 act='hard_swish',
+                 kernel_size=3,
+                 negative_slope=0.01,
+                 lr_scale=1.0,
+                 use_stem=False):
+
+        super(PicoFeatL, self).__init__()
+        self.num_convs = num_convs
+        self.norm_type = norm_type
+        self.share_cls_reg = share_cls_reg
+        self.act = act
+        self.use_stem = use_stem
+
+        feat_in = feat_in if isinstance(feat_in, (
+            list, tuple)) else [feat_in, ] * num_fpn_stride
+        feat_out = feat_out if isinstance(feat_out, (
+            list, tuple)) else [feat_out, ] * len(feat_in)
+
+        self.cls_convs = []
+        self.reg_convs = []
+        self.stem_convs = []
+
+        for stage_idx in range(num_fpn_stride):
+
+            if use_stem:
+                stem_conv = self.add_sublayer(
+                    'stem_conv{}'.format(stage_idx),
+                    ConvNormLayer(
+                        ch_in=feat_in[stage_idx],
+                        ch_out=feat_out[stage_idx],
+                        filter_size=1,
+                        stride=1,
+                        norm_type=norm_type,
+                        bias_on=False,
+                        initializer=nn.initializer.KaimingUniform(),
+                        lr_scale=lr_scale))
+                self.stem_convs.append(stem_conv)
+
+            cls_subnet_convs = []
+            reg_subnet_convs = []
+
+            for i in range(self.num_convs):
+                in_c = feat_in[stage_idx] if i == 0 else feat_out[stage_idx]
+
+                if use_stem:
+                    in_c = feat_out[stage_idx]
+
+                # cls_conv_dw = self.add_sublayer(
+                #     'cls_conv_dw{}.{}'.format(stage_idx, i),
+                #     ConvBNLayer(
+                #         in_c,
+                #         feat_out,
+                #         kernel_size,
+                #         act=self.act,
+                #         negative_slope=negative_slope))
+                # cls_subnet_convs.append(cls_conv_dw)
+
+                cls_conv_dw = self.add_sublayer(
+                    'cls_conv_dw{}.{}'.format(stage_idx, i),
+                    ConvNormLayer(
+                        ch_in=in_c,
+                        ch_out=feat_out[stage_idx],
+                        filter_size=kernel_size,
+                        stride=1,
+                        # groups=feat_out,
+                        norm_type=norm_type,
+                        bias_on=False,
+                        initializer=nn.initializer.KaimingUniform(),
+                        lr_scale=lr_scale))
+                cls_subnet_convs.append(cls_conv_dw)
+
+                # cls_conv_pw = self.add_sublayer(
+                #     'cls_conv_pw{}.{}'.format(stage_idx, i),
+                #     ConvNormLayer(
+                #         ch_in=in_c,
+                #         ch_out=feat_out,
+                #         filter_size=1,
+                #         stride=1,
+                #         norm_type=norm_type,
+                #         bias_on=False,
+                #         lr_scale=2.))
+                # cls_subnet_convs.append(cls_conv_pw)
+
+                if not self.share_cls_reg:
+                    # reg_conv_dw = self.add_sublayer(
+                    #     'reg_conv_dw{}.{}'.format(stage_idx, i),
+                    #     ConvBNLayer(
+                    #         in_c,
+                    #         feat_out,
+                    #         kernel_size,
+                    #         act=self.act,
+                    #         negative_slope=negative_slope))
+                    # reg_subnet_convs.append(reg_conv_dw)
+
+                    reg_conv_dw = self.add_sublayer(
+                        'reg_conv_dw{}.{}'.format(stage_idx, i),
+                        ConvNormLayer(
+                            ch_in=in_c,
+                            ch_out=feat_out[stage_idx],
+                            filter_size=kernel_size,
+                            stride=1,
+                            # groups=feat_out,
+                            norm_type=norm_type,
+                            bias_on=False,
+                            initializer=nn.initializer.KaimingUniform(),
+                            lr_scale=lr_scale))
+                    reg_subnet_convs.append(reg_conv_dw)
+
+                    # reg_conv_pw = self.add_sublayer(
+                    #     'reg_conv_pw{}.{}'.format(stage_idx, i),
+                    #     ConvNormLayer(
+                    #         ch_in=in_c,
+                    #         ch_out=feat_out,
+                    #         filter_size=1,
+                    #         stride=1,
+                    #         norm_type=norm_type,
+                    #         bias_on=False,
+                    #         lr_scale=2.))
+                    # reg_subnet_convs.append(reg_conv_pw)
+
+            self.cls_convs.append(cls_subnet_convs)
+            self.reg_convs.append(reg_subnet_convs)
+
+        self.negative_slope = negative_slope
+
+    def act_func(self, x):
+        if self.act == "leaky_relu" or self.act == 'leaky':
+            x = F.leaky_relu(x, negative_slope=self.negative_slope)
+        elif self.act == "hard_swish":
+            x = F.hardswish(x)
+        elif self.act == 'mish':
+            x = x * paddle.tanh(F.softplus(x))
+
+        return x
+
+    def forward(self, fpn_feat, stage_idx):
+        assert stage_idx < len(self.cls_convs)
+
+        if self.use_stem:
+            fpn_feat = self.act_func(self.stem_convs[stage_idx](fpn_feat))
+
+        cls_feat = fpn_feat
+        reg_feat = fpn_feat
+
+        for i in range(len(self.cls_convs[stage_idx])):
+            cls_feat = self.act_func(self.cls_convs[stage_idx][i](cls_feat))
+            if not self.share_cls_reg:
+                reg_feat = self.act_func(self.reg_convs[stage_idx][i](reg_feat))
+
+        return cls_feat, reg_feat
 
 
 @register
