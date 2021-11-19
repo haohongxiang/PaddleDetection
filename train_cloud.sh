5d9cf3e7f5cb27b0b047ecd44e39ef2013c43f8b
lyuwenyu <wenyu.lyu@gmail.com>
2021-11-19 11:16:36 +0800

    cloud train

---
+++ train_cloud.sh
@@ -0,0 +1,10 @@
+
+# wget http://10.181.196.20:8787/workspace/ssd6/lvwenyu01/workspace/dataset/CSPDarkNet53_pretrained.pdparams
+
+rm /dev/shm/paddle_* -rf
+
+
+ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9 
+
+python -m paddle.distributed.launch --log_dir=log   --gpus 0,1,2,3 tools/train.py -c configs/picodet/pp_fcos_cspdarknet.yml --eval  > train.log 2>&1 &
+
