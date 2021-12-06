953530d57ab3a809a846f59e3a1152ebbf073163
lyuwenyu <wenyu.lyu@gmail.com>
2021-12-06 12:38:50 +0800

    update

--- train.sh
+++ train.sh
@@ -5,3 +5,5 @@
 ps aux | grep "tools/train.py" | awk '{print $2}' | xargs kill -9 
 
 python -m paddle.distributed.launch --log_dir=log --gpus 0,1,2,3,4,5,6,7 tools/train.py -c configs/htc/htc_swin_tiny_fpn_1x_coco.yml --eval 
+
+# --eval > train.log 2>&1 &
