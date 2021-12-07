4dd9d7f2000837fc35839285650fcd4c712a2f2a
lyuwenyu <wenyu.lyu@gmail.com>
2021-12-07 20:41:52 +0800

    backbone

---
+++ ppdet/modeling/architectures/backbone.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved. 
+#   
+# Licensed under the Apache License, Version 2.0 (the "License");   
+# you may not use this file except in compliance with the License.  
+# You may obtain a copy of the License at   
+#   
+#     http://www.apache.org/licenses/LICENSE-2.0    
+#   
+# Unless required by applicable law or agreed to in writing, software   
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+# See the License for the specific language governing permissions and   
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import paddle
+from ppdet.core.workspace import register, create
+from .meta_arch import BaseArch
+
+__all__ = ['Backbone']
+
+
+@register
+class Backbone(BaseArch):
+    def __init__(
+            self,
+            backbone, ):
+        super(Backbone, self).__init__()
+        self.backbone = backbone
+
+    @classmethod
+    def from_config(cls, cfg, *args, **kwargs):
+        backbone = create(cfg['backbone'])
+        kwargs = {'input_shape': backbone.out_shape}
+
+        return {'backbone': backbone, }
+
+    def _forward(self):
+        body_feats = self.backbone(self.inputs)
+
+        return body_feats
+
+    def get_loss(self, ):
+        outputs = self._forward()
+        return outputs
+
+    def get_pred(self):
+        body_feats = self._forward()
+        output = {'bbox': None, 'bbox_num': 0}
+        return output
